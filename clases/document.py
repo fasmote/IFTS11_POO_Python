1af5a6df0ab82c2fd819495d729b2e511a2e8d7d
claudioroh <claudioroh@gmail.com>
2024-11-27 18:06:17 -0300

    cambie nombre de variables

--- clases/document.py
+++ clases/document.py
@@ -1,9 +1,9 @@
 class Document:
    
-    def __init__(self, doc_id, data):
-        self.id = doc_id  # ID único del documento
-        self.data = data  # Diccionario con los datos del documento
+    def __init__(self, doc_id, contenido):
+        self.id = doc_id
+        self.contenido = contenido 
 
     def __repr__(self):
         
-        return f"Document(ID={self.id}, Data={self.data})"
+        return f"Document(ID={self.id}, Data={self.contenido})"
