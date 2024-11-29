a6572a9db16a1fd47db499c9aa0fd1c39445885c
claudioroh <claudioroh@gmail.com>
2024-11-28 21:43:43 -0300

    arregle indentacion en str2doc.py, tambien agregue el nombre de la colecion por default y el archivo __init__.py

--- clases/str2doc.py
+++ clases/str2doc.py
@@ -1,19 +1,19 @@
-class Str2Dic():
-	def__init__(self, schema: str, separator=','):
-		if (len(schema))== 0:
-			raise SchemaError("El schema esta vacio")
+class Str2Dic:
+    def __init__(self, schema: str, separator=','):
+        if len(schema) == 0:
+            raise ValueError("El esquema está vacío")
         self.schema = schema.split(separator)
         self.separator = separator
-     def convert(self, row: str) -> dict:
+
+    def convert(self, row: str) -> dict:
         linea = row.split(self.separator)
         if len(linea) == len(self.schema):
-            i=0
-            d={}
+            i = 0
+            d = {}
             while i < len(linea):
                 d[self.schema[i]] = linea[i]
-                i = i + 1
+                i += 1
             return d
         else:
-            raise ValueError("Los campos de la fila no concuerdan con el schema")
-        
-  
+            raise ValueError("Los campos de la fila no concuerdan con el esquema")
+
